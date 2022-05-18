4622b2407f1299adacc843c4cfd67160d5e8085c
Julian Brunner <julian.brunner@gmail.com>
2022-05-18 23:05:57 +0200

    add force flag

--- zfs/library/zpool.py
+++ zfs/library/zpool.py
@@ -32,38 +32,39 @@ def zpool_set(module, name, props, current):
 	for prop, value in props.items():
 		if current[prop] != value:
 			module.run_command("zpool set {}={} {}".format(prop, print_value(value), name), check_rc = True)
-def zpool_create(module, name, vdevs, props):
+def zpool_create(module, name, force, vdevs, props):
 	if any(kind1 != "disk" and kind2 == "disk" for (kind1, _), (kind2, _) in pairwise(vdevs)):
 		module.fail_json("cannot create pool with disk vdev after group vdev", vdevs = vdevs)
 	pvdevs = " ".join(devices if kind == "disk" else " ".join([kind] + devices) for kind, devices in vdevs)
 	pprops = " ".join("-o {}={}".format(prop, print_value(value)) for prop, value in props.items())
-	module.run_command("zpool create {} {} {}".format(pprops, name, pvdevs), check_rc = True)
-def zpool_destroy(module, name):
-	module.run_command("zpool destroy {}".format(name), check_rc = True)
+	module.run_command("zpool create {} {} {} {}".format("-f" if force else "", pprops, name, pvdevs), check_rc = True)
+def zpool_destroy(module, name, force):
+	module.run_command("zpool destroy {} {}".format("-f" if force else "", name), check_rc = True)
 
-def adjust(module, name, expected, actual):
-	if expected and not actual: zpool_create(module, name, expected["vdevs"], expected["props"])
-	elif not expected and actual: zpool_destroy(module, name)
+def adjust(module, name, force, expected, actual):
+	if expected and not actual: zpool_create(module, name, force, expected["vdevs"], expected["props"])
+	elif not expected and actual: zpool_destroy(module, name, force)
 	elif expected["vdevs"] != actual["vdevs"]: module.fail_json("vdev adjustment is not supported", expected = expected, actual = actual)
 	elif expected["props"] != actual["props"]: zpool_set(module, name, expected["props"], actual["props"])
 	else: raise ValueError("impossible violation of actual vs. expected state")
 
-def process(module, name, state, vdevs, props, check):
+def process(module, name, state, force, vdevs, props, check):
 	expected = dict(vdevs = list(single(vdev.items()) for vdev in vdevs), props = props or {}) if state == "present" else None
 	vdevs = zpool_status(module, name)
 	actual = dict(vdevs = vdevs, props = zpool_get(module, name, props) if props else {}) if vdevs else None
-	if actual != expected and not check: adjust(module, name, expected, actual)
+	if actual != expected and not check: adjust(module, name, force, expected, actual)
 	return dict(changed = actual != expected, expected = expected, actual = actual)
 
 def main():
 	name = dict(type = "str", required = True)
 	state = dict(type = "str", choices = ["present", "absent"], default = "present")
+	force = dict(type = "bool", default = False)
 	vdevs = dict(type = "list", elements = "dict")
 	properties = dict(type = "dict", default = None)
-	parameters = dict(name = name, state = state, vdevs = vdevs, properties = properties)
+	parameters = dict(name = name, state = state, force = force, vdevs = vdevs, properties = properties)
 	required_if = [("state", "present", ["vdevs"])]
 	module = AnsibleModule(parameters, required_if = required_if, supports_check_mode = True)
-	result = process(module, module.params["name"], module.params["state"], module.params["vdevs"], module.params["properties"], module.check_mode)
+	result = process(module, module.params["name"], module.params["state"], module.params["force"], module.params["vdevs"], module.params["properties"], module.check_mode)
 	module.exit_json(**result)
 
 if __name__ == "__main__": main()
