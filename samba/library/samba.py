2ec9f50a6679256fc84851ed5b188317b28a3d99
Julian Brunner <julian.brunner@gmail.com>
2022-05-18 04:49:40 +0200

    simplify module logic

--- samba/library/samba.py
+++ samba/library/samba.py
@@ -20,9 +20,8 @@ def parse(line):
 
 def pdbedit_user(module, name):
 	out = run(module, "pdbedit --user {} --verbose --smbpasswd-style".format(name), check = False)
-	if not out: return dict(state = "absent", password = None)
-	entries = dict(parse(line) for line in pdbedit.stdout.splitlines())
-	return dict(state = "present", password = entries["NT hash"])
+	if not out: return None
+	return dict(parse(line) for line in pdbedit.stdout.splitlines())
 def pdbedit_create(module, name, password):
 	run(module, "pdbedit --create --user {} --password-from-stdin".format(name), "{}\n{}\n".format(password, password))
 	return "added {} with password {}".format(name, password)
@@ -34,20 +33,17 @@ def pdbedit_modify(module, name, password):
 	return "set password for {} to {}".format(name, password)
 
 def adjust(module, name, expected, actual):
-	if expected["state"] == "present" and actual["state"] == "absent": return pdbedit_create(module, name, expected["password"])
-	if expected["state"] == "absent" and actual["state"] == "present": return pdbedit_delete(module, name)
+	if expected and not actual: return pdbedit_create(module, name, expected["password"])
+	if not expected and actual: return pdbedit_delete(module, name)
 	if expected["password"] != actual["password"]: return pdbedit_modify(module, name, expected["password"])
 	raise ValueError("impossible violation of actual vs. expected state")
 
 def process(module, name, state, password, check):
-	if state == "present" and password is None:
-		raise ValueError("when state is 'present', password cannot be 'None'", state, password)
-	if state == "absent" and password is not None:
-		raise ValueError("when state is 'absent', password has to be 'None'", state, password)
-	expected = dict(state = state, password = None if password is None else hash_nt(password))
-	actual = pdbedit_user(module, name)
+	expected = dict(password = hash_nt(password)) if state == "present" else None
+	entries = pdbedit_user(module, name)
+	actual = dict(password = entries["NT hash"]) if entries else None
 	result = dict(changed = actual != expected, expected = expected, actual = actual)
-	return result | {"action": adjust(name, expected, actual)} if result["changed"] and not check else result
+	return result | {"action": adjust(module, name, expected, actual)} if result["changed"] and not check else result
 
 def main():
 	name = dict(type = "str", required = True)
