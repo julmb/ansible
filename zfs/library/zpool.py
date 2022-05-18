7a0359fdbbe4e750cf98fca5a5429788c4782fe8
Julian Brunner <julian.brunner@gmail.com>
2022-05-18 22:47:45 +0200

    use empty string for none

--- zfs/library/zpool.py
+++ zfs/library/zpool.py
@@ -11,6 +11,7 @@ def parse_value(text):
 	try: return int(text); return float(text)
 	except ValueError: return text
 def print_value(value):
+	if value is None: return ""
 	if value is False: return "off"
 	if value is True: return "on"
 	return str(value)
