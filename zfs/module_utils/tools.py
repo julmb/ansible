85d9940430a90095bbee4bd135ee4acf181a35fd
Julian Brunner <julian.brunner@gmail.com>
2022-05-18 21:20:21 +0200

    cleanup

--- zfs/module_utils/tools.py
+++ zfs/module_utils/tools.py
@@ -1,8 +1,6 @@
-# tuples
 def fst(t): x, _ = t; return x
 def snd(t): _, y = t; return y
 
-# sequences
 def single(sequence): [item] = sequence; return item
 # TODO: replace this with itertools.pairwise in python 3.10
 def pairwise(sequence): return zip(sequence, sequence[1:])
