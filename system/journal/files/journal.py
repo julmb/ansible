93cbb67220e6256d2d1212c969010f04bf57994f
Julian Brunner <julian.brunner@gmail.com>
2022-06-25 16:14:22 +0200

    cleanup

--- system/journal/files/journal.py
+++ system/journal/files/journal.py
@@ -1,18 +1,17 @@
-#!/usr/bin/python
-
 import asyncio, json, requests
 
 def notify(url, entries):
 	print("sending notification for", len(entries), "entries")
 	message = "```" + "\n".join(map(lambda entry: entry['MESSAGE'], entries)) + "```"
-	payload = {'content': message}
-	headers = {'Content-Type': 'application/json'}
-	r = requests.post(url, headers = headers, data = json.dumps(payload))
+	payload = { 'content': message }
+	r = requests.post(url, json = payload)
 	print(r)
+	print(r.text)
 
 async def journal(unit, timeout, notify):
 	print("start watching journal for", unit)
 	command = ['journalctl', '--follow', '--lines', '0', '--output', 'json', '--unit', unit]
+	# TODO: check documentation of create_subprocess_exec, see if it needs with statement and how that would look like
 	process = await asyncio.create_subprocess_exec(*command, stdout = asyncio.subprocess.PIPE)
 	entries = []
 	while True:
