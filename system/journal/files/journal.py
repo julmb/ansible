98743ae119e9005af228653712059e9c3662043b
Julian Brunner <julian.brunner@gmail.com>
2022-06-25 16:36:16 +0200

    start working on better logging

--- system/journal/files/journal.py
+++ system/journal/files/journal.py
@@ -1,9 +1,27 @@
 import asyncio, json, requests
 
+# "PRIORITY": "6"
+# "_COMM": "zpool"
+# "__REALTIME_TIMESTAMP": "1656033052945870"
+# "_SYSTEMD_UNIT": "zpool-scrub@pool2.service"
+# "MESSAGE": "\t    device  ONLINE       0     0     0"
+# "__MONOTONIC_TIMESTAMP": "274795432440"
+# "_PID": "1130150"
+# "_HOSTNAME": "host1"
+# "SYSLOG_IDENTIFIER": "sh"
+
+severities = ["Emergency", "Alert", "Critical", "Error", "Warning", "Notice", "Information", "Debug"]
+colors = [0xFF0000, 0xFF0000, 0xFF0000, 0x7F0000, 0xFFFF00, 0x00FF00, 0x000000, 0x0000FF]
+
 def notify(url, entries):
 	print("sending notification for", len(entries), "entries")
-	message = "```" + "\n".join(map(lambda entry: entry["MESSAGE"], entries)) + "```"
-	payload = { "content": message }
+	content = "```" + "\n".join(map(lambda entry: entry["MESSAGE"], entries)) + "```"
+	fields = [
+		dict(name = "Severity", value = severities[entry["PRIORITY"]]),
+		dict(name = "Identifier", value = entry["SYSLOG_IDENTIFIER"])
+	]
+	info = dict(color = colors[entry["PRIORITY"]], fields = fields)
+	payload = { "content": content, "embeds": info }
 	r = requests.post(url, json = payload)
 	print(r)
 	print(r.text)
