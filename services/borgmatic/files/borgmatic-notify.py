4da01c254f7c3e7db56054a6bb58ec25d417d035
Julian Brunner <julian.brunner@gmail.com>
2022-06-25 23:30:05 +0200

    start working on python notify script

---
+++ services/borgmatic/files/borgmatic-notify.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import sys, os, subprocess, json, time, asyncio, requests
+
+def notify(url, file, content):
+		fields = [dict(name = "File", value = file)]
+		info = dict(fields = fields)
+
+		if len(content) + 6 < 2000:
+			if len(entries) == 1:
+				payload = { "embeds": [info | dict(description = content)] }
+			else:
+				payload = { "content": "```" + content + "```", "embeds": [info] }
+			args = dict(json = payload)
+		else:
+			payload = { "embeds": [info] }
+			data = { "payload_json": json.dumps(payload) }
+			files = { "files[0]": ("borgmatic.log", content) }
+			args = dict(data = data, files = files)
+		
+		while True:
+			response = requests.post(url, **args)
+			print("response code", response.status_code)
+			print("response headers", response.headers)
+			print("response text", response.text)
+			if response.status_code == 429:
+				print("current time", datetime.datetime.now().isoformat())
+				print("reset on", datetime.datetime.utcfromtimestamp(int(response.headers["X-RateLimit-Reset"])).isoformat())
+				print("reset in", int(response.headers["X-RateLimit-Reset-After"]))
+				print("retry after", int(response.headers["Retry-After"]))
+				print("sleeping for", int(response.headers["X-RateLimit-Reset-After"]), "seconds")
+				time.sleep(int(response.headers["X-RateLimit-Reset-After"]))
+			# TODO: properly handle all error codes
+			else: break
+
+def main():
+	url = "<url>"
+
+	# for each yaml file in /etc/borgmatic.d/ run borgmatic --config <file> using subprocess.run
+	for file in os.listdir("/etc/borgmatic.d"):
+		if file.endswith(".yaml"):
+			command = ["borgmatic", "--config", "/etc/borgmatic.d/" + file, "create", "--files", "--stats"]
+			process = subprocess.run(command, output = subprocess.PIPE, text = True)
+			notify(url, file, process.stdout)
+
+main()
