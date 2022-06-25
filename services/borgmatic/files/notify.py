3c14ef0ff641d7251e77add8119a00f35a23805f
Julian Brunner <julian.brunner@gmail.com>
2022-06-25 23:58:41 +0200

    use dynamic configuration

--- services/borgmatic/files/notify.py
+++ services/borgmatic/files/notify.py
@@ -2,7 +2,9 @@
 
 import sys, os, subprocess, json, time, asyncio, requests
 
-def notify(url, content):
+def notify(name, webhook, content):
+	print("notifying", name)
+	url = "https://discord.com/api/webhooks/{}/{}".format(webhook["id"], webhook["token"])
 	if len(content) + 6 < 2000: args = dict(json = { "content": "```" + content + "```" })
 	else: args = dict(files = { "files[0]": ("borgmatic.log", content) })
 	
@@ -22,15 +24,12 @@ def notify(url, content):
 		else: break
 
 def main():
-	url = "<url>"
-
-	# TODO: enumerate configuration files from ansible configuration
-	# for each yaml file in /etc/borgmatic.d/ run borgmatic --config <file> using subprocess.run
-	for file in os.listdir("/etc/borgmatic.d"):
-		if file.endswith(".yaml"):
-			# TODO: use structured json output and generate embeds from it
-			command = ["borgmatic", "--config", "/etc/borgmatic.d/" + file, "create", "--files", "--stats"]
-			process = subprocess.run(command, capture_output = True, text = True)
-			notify(url, process.stdout)
+	with open("/etc/borgmatic-notify.json") as config: configuration = json.load(config)
+	for name, entry in configuration.items():
+		# TODO: use structured json output and generate embeds from it
+		command = ["borgmatic", "--config", "/etc/borgmatic.d/{}.yaml".format(name), "create", "--files", "--stats"]
+		print("running backup", name)
+		process = subprocess.run(command, capture_output = True, text = True)
+		notify(name, entry["webhook"], process.stdout)
 
 main()
