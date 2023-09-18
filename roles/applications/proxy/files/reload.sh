a4b9d1ceedcbe675ac93e86d501b8774a6b69d35
Julian Brunner <julian.brunner@gmail.com>
2023-09-18 20:35:35 +0200

    update notes

--- roles/applications/proxy/files/reload.sh
+++ roles/applications/proxy/files/reload.sh
@@ -7,6 +7,11 @@
 #       this is due to sleep not being interrupted by SIGTERM
 #       this can be avoided by using `sleep 24h & wait $!`
 #       this does not help, presumably because it does not break the loop
+#       https://pentacent.medium.com/nginx-and-lets-encrypt-with-docker-in-less-than-5-minutes-b4b8a60d3a71
+#       https://stackoverflow.com/questions/56981892/why-do-sleep-wait-in-bash
+#       https://stackoverflow.com/questions/360201/how-do-i-kill-background-processes-jobs-when-my-shell-script-exits
+#       http://mywiki.wooledge.org/SignalTrap#When_is_the_signal_handled.3F
+#       https://stackoverflow.com/questions/27694818/interrupt-sleep-in-bash-with-a-signal-trap
 
 while true
 do
