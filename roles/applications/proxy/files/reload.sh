7813d1f41f904dfb5c04e21486cac0bc6d92bd4d
Julian Brunner <julian.brunner@gmail.com>
2023-09-18 20:06:46 +0200

    add exit notes

--- roles/applications/proxy/files/reload.sh
+++ roles/applications/proxy/files/reload.sh
@@ -3,6 +3,11 @@
 # it executes a loop in the background that reloads the nginx configuration every 24 hours
 # docker-entrypoint.sh hands off control to nginx, orphaning the loop which will reparent
 
+# TODO: this currently does not exit cleanly when the container is stopped
+#       this is due to sleep not being interrupted by SIGTERM
+#       this can be avoided by using `sleep 24h & wait $!`
+#       this does not help, presumably because it does not break the loop
+
 while true
 do
 	sleep 24h
